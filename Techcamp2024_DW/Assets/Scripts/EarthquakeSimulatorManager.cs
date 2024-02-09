b1ca381bb051534acbbed63f38618c57c4082d76
Kiru671 <112775690+Kiru671@users.noreply.github.com>
2024-02-10 01:45:32 +0300

    Magnitude variance with time

--- Techcamp2024_DW/Assets/Scripts/EarthquakeSimulatorManager.cs
+++ Techcamp2024_DW/Assets/Scripts/EarthquakeSimulatorManager.cs
@@ -6,7 +6,8 @@ public class EarthquakeSimulatorManager : MonoBehaviour
 {
     public Transform floorTransform;
     public float frequency = 1.0f; 
-    public float magnitude = 0.5f; 
+    public float magnitude = 0.0f;
+    public float entryTime = 3f;
 
     private Vector3 originalPosition;
     private float elapsedTime = 0.0f;
@@ -17,6 +18,7 @@ public class EarthquakeSimulatorManager : MonoBehaviour
         {
             originalPosition = floorTransform.position;
         }
+        StartCoroutine("Intensify");
     }
 
     void Update()
@@ -30,6 +32,27 @@ public class EarthquakeSimulatorManager : MonoBehaviour
 
             Vector3 newPosition = originalPosition + new Vector3(x, y, z) * magnitude;
             floorTransform.position = newPosition;
+        }
+        
+    }
+    private IEnumerator Intensify() 
+    {
+        while (true) 
+        {
+            switch (magnitude) 
+            {
+                case float n when (n < 1):
+                    magnitude += 0.1f;
+                    break;
+                case float n when (n >= 1 && n < 2):
+                    magnitude += 0.025f;
+                    break;
+                case float n when (n > 2):
+                    magnitude -= 0.2f;
+                    break;         
+            }
+            Debug.Log(magnitude);   
+            yield return new WaitForSeconds(0.2f);
         }
     }
 }
